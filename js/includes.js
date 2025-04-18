302fb3de7b1e43c77c7bd22cbf52ea930ec8e8ec
Jhoel Alonso <jtinipuclla2@gmail.com>
2025-04-18 16:34:00 -0500

    Navbar y favicon

---
+++ js/includes.js
@@ -0,0 +1,10 @@
+// This script loads the navbar and footer HTML into the main page
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("partials/navbar.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("navbar").innerHTML = data;
+    })
+    .catch(error => console.error("Error loading navbar:", error));
+});
